7b265de5b5e4ddd23994913ad27946f46959defe
Zizhang He <hzz1996@qq.com>
2025-02-24 09:33:06 -0600

    initial commit

---
+++ test.py
@@ -0,0 +1,24 @@
+# Missing colon in function definition
+def broken_function()
+print("This is broken!")
+
+# Indentation error
+ def bad_indent():
+print("Wrong indentation!")
+
+# Using an undefined variable
+print(undefined_variable)
+
+# Syntax error: Mismatched brackets
+list_example = [1, 2, 3
+
+# Incorrect string concatenation
+name = "Alice"
+message = "Hello, " + name + 5
+
+# Attempting to return outside of a function
+return "This won't work!"
+
+# Incorrect use of assignment inside an if condition
+if x = 10:
+        print("This is incorrect!")
